4a4240bf81d28d20b7a63c8f2faf1a259b1ecd78
fredlc <fredlefevre@outlook.com>
2020-02-08 20:00:34 -0500

    Refactored image upload buttons styles and placeholder method

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+		"https://via.placeholder.com/#{height}x#{width}"
+  end
+  
+  def portfolio_image image, type
+    if image.model.main_image? || image.model.thumb_image?
+      image
+    elsif type == 'thumb'
+      image_generator(height: '200', width: '350')
+    else
+      image_generator(height: '400', width: '600')
+    end
+  end
 end
